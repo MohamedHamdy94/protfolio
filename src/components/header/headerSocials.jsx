3d324543a6a272d0d95b4dfb19cc1ee76606a024
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2022-05-08 15:35:46 +0200

    edite portfolio

--- src/components/header/headerSocials.jsx
+++ src/components/header/headerSocials.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BsLinkedin} from "react-icons/bs"
 import {FaGithub} from "react-icons/fa"
 import {FiDribbble} from "react-icons/fi"
+import { FaFacebookF } from 'react-icons/fa';
 
 const HeaderSocials = () => {
   return (
@@ -9,7 +10,9 @@ const HeaderSocials = () => {
       <a href="https://www.linkedin.com/in/mohamed-hamdy-ahmed-mohamed-138693219/" target="_blank"><BsLinkedin/></a>
       
       <a href="https://github.com/MohamedHamdy94" target="_blank"><FaGithub/></a>
-
+      <a href="https://www.facebook.com/anakingmsr/">
+          <FaFacebookF />
+        </a>
       {/* <a href="linked" target="_blank"><FiDribbble/></a> */}
 
     </div>
